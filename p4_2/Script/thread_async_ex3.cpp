1236e39d6ef34035c7414139646726aa8dc43117
Pofu Wu <bn8524372@gmail.com>
2021-11-25 23:23:22 +0800

    Update Threads vs Task (async & thread)

---
+++ p4_2/Script/thread_async_ex3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <cmath>
+#include <vector>
+#include <chrono>
+
+void workerFunction(int n)
+{
+    // print system id of worker thread
+    std::cout << "Worker thread id = " << std::this_thread::get_id() << std::endl;
+
+    // perform work
+    for (int i = 0; i < n; ++i)
+    {
+        sqrt(12345.6789);
+    }
+}
+
+int main()
+{
+    // print system id of worker thread
+    std::cout << "Main thread id = " << std::this_thread::get_id() << std::endl;
+
+    // start time measurement
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+    
+    // launch various tasks
+    // 1. int nLoops = 1e7 , nThreads = 4 , std::launch::async
+    // 2. int nLoops = 1e7 , nThreads = 5 , std::launch::deferred
+    // 3. int nLoops = 10 , nThreads = 5 , std::launch::async
+    // 4. int nLoops = 10 , nThreads = 5 , std::launch::deferred
+    std::vector<std::future<void>> futures;
+    int nLoops = 10, nThreads = 5;
+    for (int i = 0; i < nThreads; ++i)
+    {
+        //change: std::launch::any -> std::launch::async or std::launch::deferred
+        futures.emplace_back(std::async(std::launch::deferred, workerFunction, nLoops));
+    }
+
+    // wait for tasks to complete
+    for (const std::future<void> &ftr : futures)
+        ftr.wait();
+
+    // stop time measurement and print execution time
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    std::cout << "Execution finished after " << duration <<" microseconds" << std::endl;
+    
+    return 0;
+}
