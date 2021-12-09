72713086f167be273b47333d408e448f817fdd07
Pofu Wu <bn8524372@gmail.com>
2021-12-09 13:55:06 +0800

    Add thread_Safe.cpp

---
+++ p4_3/Script/thread_safe.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+using namespace std;
+std::mutex mu;
+void CallHome(string message)
+{
+  std::lock_guard<std::mutex> glock(mu);
+  cout << "Thread " << this_thread::get_id() << " says " << message << endl;
+}
+int main()
+{
+  thread t1(CallHome, "Hello from Jupiter");
+  thread t2(CallHome, "Hello from Pluto");
+  thread t3(CallHome, "Hello from Moon");
+  CallHome("Hello from Main/Earth");
+  thread t4(CallHome, "Hello from Uranus");
+  thread t5(CallHome, "Hello from Neptune");
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+  t5.join();
+  return 0;
+}
